5c156b7c9317ae97f78285da3679c860a3c698a7
emr144 <167718927+emr144@users.noreply.github.com>
2024-06-16 17:07:54 -0300

    Chat andando y ahora voy a preparar el videro

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,21 @@
-# chat/views.py
-from django.shortcuts import render
+# chatapp/views.py
 
+from django.shortcuts import render, redirect
+from .forms import MessageForm
+from .models import Message
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def index(request):
-    return render(request, 'chat.html')
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.username = request.user.username
+            message.save()
+            return redirect('chat')
+    else:
+        form = MessageForm()
+
+    messages = Message.objects.all().order_by('-timestamp')
+    return render(request, 'chat.html', {'form': form, 'messages': messages})
